235812db14060ebe8a0a14b2ab8204d2018fae04
Joosemi02 <josemiguel.dark@gmail.com>
2023-06-23 15:43:48 +0200

    Move helper functions into background.js

--- background.js
+++ background.js
@@ -27,3 +27,17 @@ chrome.runtime.onInstalled.addListener(() => {
     }]);
   });
 });
+
+const getFromStorage = keys => new Promise((resolve) =>
+  chrome.storage.sync.get(keys, result => resolve(result)));
+
+const generateUrl = async (selectedText) => {
+  const {
+    sourceLang = 'en',
+    targetLang = 'de',
+  } = await getFromStorage(['sourceLang', 'targetLang']);
+
+  return selectedText === ""
+    ? `https://www.deepl.com/translator#${sourceLang}/${targetLang}`
+    : `https://www.deepl.com/translator#${sourceLang}/${targetLang}/${selectedText}`;
+};
